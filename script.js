c9c2c220e7ea79ff0cabf7da4da3d91282abf1fb
piyushsingh8373 <122162309+piyushsingh8373@users.noreply.github.com>
2025-07-04 13:58:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function scrollToTop() {
-  const c = document.documentElement.scrollTop || document.body.scrollTop;
-  if (c > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    window.scrollTo(0, c - c / 8);
-  }
-}
-
 const htmlBody = document.querySelector("html, body");
 const results = document.querySelector("#results");
 
